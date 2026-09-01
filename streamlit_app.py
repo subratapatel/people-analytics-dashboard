9eca06f09fc60b845c496dd6bf53e96fe6ccf5e5
Subrata Kumar Patel <subratapatel81@gmail.com>
2026-09-01 16:36:33 +0530

    Reduce gap between bar groups in chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -277,7 +277,7 @@ def create_bar_chart(chart_data):
         bargap=0.40,
 
         # Small gap between New Joiners and Exits
-        bargroupgap=0.08,
+        bargroupgap=0.04,
 
         # Legend
         legend=dict(
